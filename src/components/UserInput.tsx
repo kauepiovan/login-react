770460e7f601982a4d5921b4fd8022bd638d80f1
Kaue Piovan <kaupiovan@gmail.com>
2025-05-27 21:08:27 -0300

    finalizando a tela de login ( aparencia inicial )

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -4,15 +4,18 @@ function UserInput() {
   return (
     <>
       <div className="login">
+        <h2>login</h2>
+        <h4>entre com sua conta</h4>
         <form action="">
           <label htmlFor="input-email">
             <p>usuario</p>
-            <input type="email" name="email" id="input-email" />
+            <input type="email" name="email" id="input-email" required />
           </label>
           <label htmlFor="input-password">
             <p>senha</p>
-            <input type="password" name="password" id="input-password" />
+            <input type="password" name="password" id="input-password" required />
           </label>
+          <button id='button-entrar'>entrar</button>
         </form>
       </div>
     </>
